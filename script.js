ced0b2e09f3cdf9cd922731f5e3a9d224aa814ae
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-09 18:47:19 +0100

    Create UI for calculator with html and css

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World!");
+// Create functions for basic math operations
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+function operate(a, b, operator) {
+  let result;
+  if (operator === "+") {
+    result = add(a, b);
+  } else if (operator === "-") {
+    subtract(a, b);
+  } else if (operator === "*") {
+    result = multiply(a, b);
+  } else if (operator === "/") {
+    result = divide(a, b);
+  }
+
+  return result;
+}
