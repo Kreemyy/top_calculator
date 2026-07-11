ac905a3ac5c80eb0cf4ee76a5c4849757cd8d7a4
Kareem <abdulkareemogedemgbe@outlook.com>
2026-07-11 12:19:59 +0100

    Create functions for basic maths operations
    Create a function to update the operands of the maths expression
    according to buttons clicked. Add click event listener on the buttons
    container and use event delegation to get target button on the
    calculator. Classify buttons as operators, function keys, number keys to
    update operands and maths expression accordingly.
    create a function to operate on two operands according to the operator
    between the two operands i.e add, subtract, multiply, divide.

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function divide(a, b) {
   return a / b;
 }
 
-let firstOperand;
-let secondOperand;
-let operator;
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
 
 function operate(a, b, operator) {
   let result;
@@ -34,3 +34,47 @@ function operate(a, b, operator) {
 
   return result;
 }
+
+let isSecondOperand = false;
+let isFirstOperand = false;
+let isOperating = false;
+
+function updateExpression(number, isOperator, isFunctionKey) {
+  if (firstOperand !== "" && operator === "" && isOperator) {
+    operator = number;
+    isOperating = true;
+  }
+
+  if (isOperating && !isOperator && !isFunctionKey) {
+    secondOperand += number;
+    isSecondOperator = true;
+  } else if (!isOperator && !isFunctionKey) {
+    firstOperand += number;
+  }
+}
+
+const digitBtn = document.querySelector(".btns");
+digitBtn.addEventListener("click", (event) => {
+  const calculation = document.querySelector(".calculation");
+  const operand = String(event.target.textContent);
+
+  let isOperator = false;
+  let isFunctionKey = false;
+
+  if (
+    operand === "-" ||
+    operand === "+" ||
+    operand === "×" ||
+    operand === "÷"
+  ) {
+    isOperator = true;
+  } else if (operand === "Clear" || operand == "Delete") {
+    isFunctionKey = true;
+  }
+
+  updateExpression(operand, isOperator, isFunctionKey);
+
+  console.log(
+    `firstOp: ${firstOperand} Op: ${operator} secondOp: ${secondOperand}`,
+  );
+});
